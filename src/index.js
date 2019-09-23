e21b6614a09fd2790f7b03de26f8e8fbb6a57185
bhavanapanduru <p.bhavana1997@gmail.com>
2019-09-23 14:56:17 +0530

    Add route to login component

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 import "./index.css";
 import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
-
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from "./components/Auth/Login";
 
 const Root = () => (
     <Router>
         <Switch>
-            <Route path="/" component={App}></Route>
+            <Route exact path="/" component={App}></Route>
+            <Route path="/login" component={Login}></Route>
         </Switch>
     </Router>
 );
