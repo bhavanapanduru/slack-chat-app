e0ae073566e6ca9bf1c5d281176db2f1f900ca78
bhavanapanduru <p.bhavana1997@gmail.com>
2019-09-23 16:04:50 +0530

    Add route for Register component

--- src/index.js
+++ src/index.js
@@ -6,12 +6,14 @@ import "./index.css";
 import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
 import Login from "./components/Auth/Login";
+import Register from "./components/Auth/Register";
 
 const Root = () => (
     <Router>
         <Switch>
             <Route exact path="/" component={App}></Route>
             <Route path="/login" component={Login}></Route>
+            <Route path="/register" component={Register}></Route>
         </Switch>
     </Router>
 );
